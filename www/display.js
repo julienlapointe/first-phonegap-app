fba9d70031e050c175c01fb95897bfd6ddbf86f9
Julien LaPointe <email@julienlapointe.com>
2017-04-21 10:42:35 -0400

    class demo version

--- www/display.js
+++
@@ -1,8 +0,0 @@
-var display = new Object();
-display.stage = null;
-display.queue = null;
-display.hitAnimation = null;
-display.idleAnimation = null;
-display.laughingAnimation = null;
-display.popAnimation = null;
-display.teaseAnimation = null;
--- www/globals.js
+++
@@ -1,8 +0,0 @@
-var globals = new Object();
-globals.level = 1;
-globals.gameIntv = null;
-globals.playing = false;
-globals.gameTime = 0;
-globals.holePositions= null;
-globals.score = 0;
-globals.scoreText = null;
