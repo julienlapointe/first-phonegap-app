a8b3307740aa7d59444198866395c411b774ccfc
Julien LaPointe <email@julienlapointe.com>
2017-04-21 12:15:06 -0400

    final commit

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,7 +1,35 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     StatusBar.overlaysWebView(false);
+    $.mobile.orientationChangeEnabled = false;
 }
+
+$(document).on('swipeleft', '.ui-page', function(event){    
+    if(event.handled !== true) // This will prevent event triggering more then once
+    {    
+        var nextpage = $.mobile.activePage.next('[data-role="page"]');
+        // swipe using id of next page if exists
+        if (nextpage.length > 0) {
+            $.mobile.changePage(nextpage, {transition: "slide", reverse: false}, true, true);
+        }
+        event.handled = true;
+    }
+    return false;         
+});
+
+$(document).on('swiperight', '.ui-page', function(event){     
+    if(event.handled !== true) // This will prevent event triggering more then once
+    {      
+        var prevpage = $(this).prev('[data-role="page"]');
+        if (prevpage.length > 0) {
+            $.mobile.changePage(prevpage, {transition: "slide", reverse: true}, true, true);
+        }
+        event.handled = true;
+    }
+    return false;            
+});
+
+
 
 
 var scoreBoard = "";
@@ -111,10 +139,6 @@ function startGame()
 // $(document).on("pageshow", "#gameplay-screen", function() 
 window.onload = function() 
 {
-	$.mobile.changePage("#welcome-screen"+"#gameplay-screen", {
-        transition: "slide",
-        reverse: true
-    });
 	boxes = $('.box');
 	boxes = [].slice.call($('.box'));
 	console.log(boxes);
